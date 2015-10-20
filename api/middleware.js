763b9278c53c3d625c5044179bf6add4d38a4796
Jarod Reyes <jarodreyes@gmail.com>
2015-10-20 14:10:21 -0700

    A sort is sort of just a sort.

--- api/middleware.js
+++ api/middleware.js
@@ -38,15 +38,8 @@ exports.loginRequired = function(request, response, next) {
 };
 
 function sortObject(object){
-    var sortedObj = {},
-    var keys = Object.keys(object);
-
-    keys.sort(function(key1, key2){
-        key1 = key1.toLowerCase(), key2 = key2.toLowerCase();
-        if(key1 < key2) return -1;
-        if(key1 > key2) return 1;
-        return 0;
-    });
+    var sortedObj = {};
+    var keys = Object.keys(object).sort();
 
     for(var index in keys){
         var key = keys[index];
@@ -60,7 +53,7 @@ function sortObject(object){
 }
 
 // Authenticate Authy request
-exports.validateSignature = function(request, reponse, next) {
+exports.validateSignature = function(request, response, next) {
     var key = config.authyApiKey;
     var url = request.protocol + '://' + request.get('host') + request.originalUrl;
     var params = Qs.stringify(sortObject(request.body));
